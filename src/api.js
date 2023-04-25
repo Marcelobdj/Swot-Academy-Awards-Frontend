dc7ed4ed5e7d1ce445bebd7fb66612e9fe2c4245
Marcelo Di Jorge <marcelobdij@gmail.com>
2023-04-24 21:20:54 -0300

    feat: add a correction to the api url after deploy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:7000/api',
+    baseURL: 'http://206.189.200.26:7000/api',
 });
 
 // Categories API functions
